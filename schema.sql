bcd643d7f2639df6e99cf00aca746250e164373d
Girish <giryagr8@gmail.com>
2020-01-19 03:45:12 +0530

    Major changes

--- schema.sql
+++
@@ -1,9 +0,0 @@
-drop table if exists users;
-    create table users (
-    id integer primary key autoincrement,
-    username text not null,
-    password text not null,
-    contact text not null,
-    email text not null,
-    usertype text not null
-);
