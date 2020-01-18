d0870293f711f50046ce9fc20283db1e328a8a0c
rahulmistry33 <priyarahul0307@gmail.com>
2020-01-18 21:48:33 +0530

    added login route

--- server.py
+++ server.py
@@ -1,10 +1,23 @@
-from flask import Flask, redirect, url_for, render_template, request, session, flash
+from flask import Flask, redirect, url_for, render_template, request, session, flash,Response
+from agora_community_sdk import AgoraRTC
+
+
 # Creating instance of Flask...
 app = Flask(__name__)
 
 @app.route("/index")
 def index():
-	return render_template("index.html")
+    
+    return render_template("index.html")
+
+@app.route("/login")
+def login():
+    return render_template('login.html')
+
+
+
+
+
 
 if __name__ == '__main__':
 
