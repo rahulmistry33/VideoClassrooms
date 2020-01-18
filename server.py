ae21041a0b886060ee2a2223f5ed90681d0f4ca8
Girish <giryagr8@gmail.com>
2020-01-18 13:10:56 +0530

    Flask added

---
+++ server.py
@@ -0,0 +1,10 @@
+from flask import Flask, redirect, url_for, render_template, request, session, flash
+# Creating instance of Flask...
+app = Flask(__name__)
+
+@app.route("/index")
+def index():
+	return render_template("index.html")
+
+if __name__ == '__main__':
+	app.run(debug=True)
