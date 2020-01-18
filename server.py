ae77dae8ccf06a59c14c72b790f658b991a87133
Prachit99 <prachitmhalgi@gmail.com>
2020-01-19 03:51:03 +0530

    Dashboard remastered

--- server.py
+++ server.py
@@ -1,5 +1,5 @@
 from flask import Flask, redirect, url_for, render_template, request, session, flash,Response
-from agora_community_sdk import AgoraRTC
+#from agora_community_sdk import AgoraRTC
 # import models as dbHandler
 from flask_sqlalchemy import SQLAlchemy
 import os
