59a832711a5c00ff6c0a48cdfdc8e34e643f617c
rahulmistry33 <priyarahul0307@gmail.com>
2020-01-19 02:11:06 +0530

    'Changes'

--- server.py
+++ server.py
@@ -2,31 +2,52 @@ from flask import Flask, redirect, url_for, render_template, request, session, f
 from agora_community_sdk import AgoraRTC
 # import models as dbHandler
 from flask_sqlalchemy import SQLAlchemy
+import os
 
 
 
 # Creating instance of Flask...
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///example.sqlite"
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'db.sqlite')
+
 db = SQLAlchemy(app)
 
-# class User(db.Model):
-# 	username = db.Column(db.String(40), primary_key = True, nullable=False)
-# 	email = db.Column(db.String(100), unique=True, nullable=False)
-# 	name = db.Column(db.String(), nullable=False)
-# 	password = db.Column(db.String(), nullable=False)
-#     phone = db.Column(db.String())
-#     usertype = db.Column(db.String())
-	
-
+class User(db.Model):
+    username = db.Column(db.String(40), primary_key = True, nullable=False)
+    email = db.Column(db.String(100), unique=True, nullable=False)
+    name = db.Column(db.String(), nullable=False)
+    password = db.Column(db.String(), nullable=False)
+    contact = db.Column(db.String())
+    usertype = db.Column(db.String())
 
 @app.route("/index")
 def index():
-    
     return render_template("index.html")
 
-@app.route("/login",methods=['GET','POST'])
-def login():
+def permit_login(username, password):
+    try:
+        user = User.query.filter_by(username=username).first()
+        if user.password == password:
+            print("Matched")
+            
+            return redirect(url_for('dashboard'))
+        else:
+            return render_template("login.html")
+    except:
+        print("error")
+        return redirect(url_for("login"))
+
+
+@app.route('/dashboard')
+def dashboard():
+    print("Dashboard")
+    return render_template("dashboard.html")
+
+@app.route("/register",methods=['GET','POST'])
+def register():
     if request.method == 'POST':
         name = request.form['name']
         usertype = request.form.get('value')
@@ -34,23 +55,21 @@ def login():
         password = request.form['password']
         email = request.form['email']
         contact = request.form['contact']
-        # dbHandler.insertUser(name,username, password,usertype,email,contact)
+        user = User(username=username, email=email, name=name, password=password, contact=contact, usertype=usertype)
+        db.session.add(user)
+        db.session.commit()
         return redirect('index')
    		# users = dbHandler.retrieveUsers()
+    return render_template('login.html')
 
-
+@app.route("/login",methods=['GET','POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        return permit_login(username,password)
 
     return render_template('login.html')
 
-
-
-
-
-
-
-
-
-if __name__ == '__main__':
-
-	app.run(debug=True)
-
+if __name__ == "__main__":
+    app.run(debug=True)
